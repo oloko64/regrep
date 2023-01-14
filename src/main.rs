b7fb768fcf92e18526c26078c494497a3a087c92
OLoKo64 <naldoepingo1@gmail.com>
2023-01-13 21:42:10 -0300

    feat: improve code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
-use rayon::prelude::*;
+// use rayon::prelude::*;
 use std::{
     io::{self, BufRead},
     process::exit,
-    sync::Mutex,
+    // sync::Mutex,
 };
 
-#[derive(Clone)]
-struct LineMatch {
+struct Matches {
     line: String,
-    line_number: i64,
+    line_number: usize,
+}
+
+impl Matches {
+    fn new(line: &str, line_number: usize, matcher: &str) -> Self {
+        Self {
+            line: line.replace(matcher, &format!("\x1b[94m{matcher}\x1b[0m")),
+            line_number,
+        }
+    }
 }
 
 fn main() {
-    match match_in_file(&read_lines_stdin(), &get_match_arg()) {
-        Some(matched) => {
-            for line in matched {
-                println!("{}: {}", line.line_number, line.line);
-            }
+    if let Some(matched) = match_in_file(&read_lines_stdin(), &get_match_arg()) {
+        for line in matched {
+            println!("{}: {}", line.line_number, line.line);
         }
-        None => (),
     }
 }
 
@@ -27,31 +32,48 @@ fn read_lines_stdin() -> Vec<String> {
         .lock()
         .lines()
         .into_iter()
-        .map(|line| line.unwrap())
+        .map(Result::unwrap)
         .collect()
 }
 
-fn match_in_file(lines: &Vec<String>, matcher_list: &Vec<String>) -> Option<Vec<LineMatch>> {
-    let matches = Mutex::new(Vec::new());
-    lines.par_iter().enumerate().for_each(|(index, line)| {
-        matcher_list.par_iter().for_each(|matcher| {
+fn match_in_file(lines: &[String], matcher_list: &[String]) -> Option<Vec<Matches>> {
+    let mut matches = Vec::new();
+    for (index, line) in lines.iter().enumerate() {
+        for matcher in matcher_list {
             if line.contains(matcher) {
-                let mut guard = matches.lock().unwrap();
-                guard.push(LineMatch {
-                    line: line
-                        .to_string()
-                        .replace(matcher, &format!("\x1b[94m{}\x1b[0m", matcher)),
-                    line_number: index as i64 + 1,
-                });
+                matches.push(Matches::new(line, index, matcher));
             }
-        });
-    });
-    if matches.lock().unwrap().is_empty() {
+        }
+    }
+    if matches.is_empty() {
         None
     } else {
-        Some(matches.lock().unwrap().to_vec())
+        Some(matches)
     }
 }
+
+// Just an example using parallel iterators
+// fn match_in_file(lines: &Vec<String>, matcher_list: &Vec<String>) -> Option<Vec<LineMatch>> {
+//     let matches = Mutex::new(Vec::new());
+//     lines.par_iter().enumerate().for_each(|(index, line)| {
+//         matcher_list.par_iter().for_each(|matcher| {
+//             if line.contains(matcher) {
+//                 let mut guard = matches.lock().unwrap();
+//                 guard.push(LineMatch {
+//                     line: line
+//                         .to_string()
+//                         .replace(matcher, &format!("\x1b[94m{}\x1b[0m", matcher)),
+//                     line_number: index as i64 + 1,
+//                 });
+//             }
+//         });
+//     });
+//     if matches.lock().unwrap().is_empty() {
+//         None
+//     } else {
+//         Some(matches.lock().unwrap().to_vec())
+//     }
+// }
 
 fn get_match_arg() -> Vec<String> {
     let args: Vec<String> = std::env::args().collect();
